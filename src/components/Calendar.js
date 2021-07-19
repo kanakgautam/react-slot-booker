e7e762b21c29e71283e59e8b8862210ea0f99b10
kanak gautam <kanakgautam5@gmail.com>
2021-07-19 22:24:04 +0530

    hh

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -7,7 +7,7 @@ import img3 from '../images/Rectangle.png'
 import Month from './Month'
 function Calendar() {
     const weekDays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
-    const monthDays = [0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0,];
+    const monthDays = [0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0,0];
     const [active, setActive] = useState(-1);
     const handelclick = (item) => {
         console.log(item);
